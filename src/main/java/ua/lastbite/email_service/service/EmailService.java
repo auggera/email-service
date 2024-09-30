8e25c2764eeb0bc0cf10db5eb3e7a36d889f8e0d
Augustin Gerasym <augustingerasym@gmail.com>
2024-09-30 22:46:42 +0200

    Improved the structure of the methods.

--- src/main/java/ua/lastbite/email_service/service/EmailService.java
+++ src/main/java/ua/lastbite/email_service/service/EmailService.java
@@ -1,9 +1,15 @@
 package ua.lastbite.email_service.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import ua.lastbite.email_service.dto.EmailRequest;
+import ua.lastbite.email_service.dto.EmailVerificationRequest;
 
 @Service
 public class EmailService {
@@ -11,19 +17,27 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
-    public void sendSimpleEmail(String toEmail, String subject, String body) {
+    @Value("${}")
+    private String verificationBaseUrl;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
+
+    @Async
+    public void sendSimpleEmail(EmailRequest request) {
+        LOGGER.info("Sending email to {}", request.getToEmail());
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject(subject);
-        message.setText(body);
+        message.setTo(request.getToEmail());
+        message.setSubject(request.getSubject());
+        message.setText(request.getBody());
         mailSender.send(message);
+        LOGGER.info("Email sent successfully to {}", request.getToEmail());
     }
 
-    public void sendVerificationEmail(String toEmail, String token) {
+    public void sendVerificationEmail(EmailVerificationRequest request) {
         String subject = "Email Verification";
-        String verificationUrl = "http://localhost:8080/api/users/verify-email?token=" + token;
+        String verificationUrl = verificationBaseUrl + "/api/users/verify-email?token=" + request.getToken();
         String body = "Please click the following link to verify your email: " + verificationUrl;
 
-        sendSimpleEmail(toEmail, subject, body);
+        sendSimpleEmail(new EmailRequest(request.getToEmail(), subject, body));
     }
 }
