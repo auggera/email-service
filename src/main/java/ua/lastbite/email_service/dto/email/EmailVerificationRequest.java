aaeaa59c51cac205dbe2ffb73db271c306dbfbde
Augustin Gerasym <augustingerasym@gmail.com>
2024-11-02 18:11:26 +0100

    Updated EmailVerificationRequest.java. Removed redudant field toEmail.

--- src/main/java/ua/lastbite/email_service/dto/email/EmailVerificationRequest.java
+++ src/main/java/ua/lastbite/email_service/dto/email/EmailVerificationRequest.java
@@ -1,6 +1,5 @@
 package ua.lastbite.email_service.dto.email;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
@@ -10,12 +9,6 @@ import lombok.*;
 @Setter
 @EqualsAndHashCode
 public class EmailVerificationRequest {
-
-    @NotBlank(message = "Email cannot be empty")
-    private String toEmail;
-
-    @NotBlank(message = "token cannot be empty")
-    private String token;
 
     @NotNull(message = "User ID cannot be empty")
     private Integer userId;
